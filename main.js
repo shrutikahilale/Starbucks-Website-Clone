d787ca66f2f7b53823c4a3ce223b7f019ec4c207
shrutikahilale <shrutikahilale8@gmail.com>
2023-02-12 19:38:26 +0530

    added responsive with more funcitonality

---
+++ main.js
@@ -0,0 +1,67 @@
+let hamburgerMenu = document.getElementById('hamburger-menu');
+let isMenuClose= true;
+
+hamburgerMenu.addEventListener("click", ()=>{
+    let fl = document.getElementById('fl');
+    let sl = document.getElementById('sl');
+    let tl = document.getElementById('tl');
+
+    let menu = document.getElementById('menu-bar');
+
+    let body = document.getElementsByTagName('body')[0];
+
+    if(isMenuClose == true) {
+        fl.style.transform = "translateY(6px) rotate(135deg)";        
+        sl.style.visibility = "hidden";        
+        tl.style.transform = " translateY(-6px) rotate(45deg)";    
+        
+        // open menu
+        menu.style.transform = "translateX(-600px)"
+
+        isMenuClose = false;
+
+        // stop vertical scroll
+        body.style.overflowY = "hidden";
+    }
+    else {
+        fl.style.transform = "translateY(0px) rotate(0deg)";        
+        sl.style.visibility = "visible";        
+        tl.style.transform = " translateY(0px) rotate(0deg)";
+
+        // close menu
+        menu.style.transform = "translateX(0px)";
+
+        // let scroll vertically now!
+        body.style.overflowY = "scroll";
+        isMenuClose = true;
+    }
+    
+});
+
+
+// open drop down menu
+let dropDown = document.getElementsByClassName('contact-heading');
+
+for(let i=0;i<dropDown.length;i++){
+    let isDropDownOpen = false;
+
+    dropDown[i].addEventListener('click', function(event){
+        let e = event.target;
+        let contactLinkDiv = e.parentElement.nextElementSibling;
+
+        if(isDropDownOpen == false) {
+            e.style.transform = "rotateZ(180deg)";
+        
+            contactLinkDiv.classList.add('show_');
+            contactLinkDiv.classList.remove('hide_'); 
+            isDropDownOpen = true;  
+        }
+        else {
+            e.style.transform = "rotateZ(0deg)";
+
+            contactLinkDiv.classList.add('hide_');
+            contactLinkDiv.classList.remove('show_'); 
+            isDropDownOpen = false;  
+        }
+    });
+};
